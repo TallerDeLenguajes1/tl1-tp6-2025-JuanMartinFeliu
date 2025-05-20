3e0be02736b093eee21e1062c8042eb1f780def0
Juan Martin Feliu <jmfeliu04@gmail.com>
2025-05-20 12:47:54 -0300

    Commit hecho en la clase

---
+++ Ejercicios/Ejercicio_1/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.WriteLine("Ingrese el valor del numero a invertir:");
+int x = Convert.ToInt32(Console.ReadLine());
+int resultado = 0;
+while (x > 0)
+{
+    resultado = resultado * 10 + x % 10;
+    x = x / 10;
+}
+Console.WriteLine("El numero invertido es:" + resultado);
